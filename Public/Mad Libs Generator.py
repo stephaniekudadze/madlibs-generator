d1cd8656649f4a76385fc55f7c75831d7855b88e
stephaniekudadze <stephaniekd72@gmail.com>
2024-06-11 10:24:46 +0000

    Initial Commit

---
+++ Public/Mad Libs Generator.py
@@ -0,0 +1,13 @@
+# Mad Libs Generator
+
+#User inputs
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+adverb = input("Enter and adverb: ")
+
+#Mad Libs story
+story = f"Once upon a time, there was a {adjective} {noun} that loved to {verb} {adverb}."
+
+#Display the story
+print(story)
